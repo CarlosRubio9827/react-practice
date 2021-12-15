3ac1f1f269ca6e9cdc6b0beca1ce4fee673c4201
CarlosRubio9827 <rubiogallegoc@gmail.com>
2021-12-14 21:25:53 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App'
+
+
+ReactDOM.render(
+    
+    <React.Fragment>
+        <App/>
+        <div>
+        dasda
+        </div>
+    </React.Fragment>
+    , document.getElementById('app')
+    );
